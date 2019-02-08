4c4b2795e39c74c1d88556f6cce0443fb85b37d2
CibiAananth <cibiaananth@gmail.com>
2019-02-09 00:20:36 +0530

    added commands to generate CSV files for system details and workstation service details

--- script.ps1
+++ script.ps1
@@ -1,16 +1,46 @@
-$TextFilePath = Read-Host "What is the path to the text file?"
+$TextFilePath = Read-Host "What is the path to the computer list text file?"
+$OutputCSVFilePath = Read-Host "Where do you want to save the CSV files (path)?"
+$SystemCSVPath = Join-Path $OutputCSVFilePath "system_information.csv"
+$ServiceCSVPath = Join-Path $OutputCSVFilePath "service_information.csv"
+
 If (Test-Path $TextFilePath) {
     $ComputersArray = Get-Content $TextFilePath
-    ForEach ($Computer in $ComputersArray) {
+
+    $SystemInformation = ForEach ($Computer in $ComputersArray) {
         If (Test-Connection $Computer -Count 1) {
-            Write-Host($Computer)
-            Invoke-Command -ComputerName $Computer {dir c:/}
+            $System = Get-WmiObject Win32_ComputerSystem -ComputerName $Computer | Select-Object -Property Name, Model
+            $BIOS = Get-WmiObject Win32_BIOS -ComputerName $Computer | Select-Object -Property SerialNumber
+            [PSCustomObject]@{
+                ComputerName = $Computer
+                Name         = $System.Name
+                Model        = $System.Model
+                SerialNumber = $BIOS.SerialNumber
+            }
         }
         Else {
             Write-Host("$Computer appears to be offline.")
         }
     }
+    Write-Host("Saving SystemInformation CSV file in the location", $SystemCSVPath)
+    $SystemInformation | Export-Csv -Path $SystemCSVPath -NoTypeInformation
+  
+    $ServiceInformation = ForEach ($Computer in $ComputersArray) {
+        If (Test-Connection $Computer -Count 1) {
+            $Service = Get-Service LanmanWorkstation | Select-Object -property name, status
+            $Mode = Get-WmiObject -Query "Select StartMode From Win32_Service Where Name='LanmanWorkStation'" | Select-Object -property StartMode
+            [PSCustomObject]@{
+                ComputerName      = $Computer
+                WorkstationStatus = $Service.Status
+                StartupMode       = $Mode.StartMode
+            }
+        }
+        Else {
+            Write-Host("$Computer appears to be offline.")
+        }
+    }
+    Write-Host("Saving ServiceInformation CSV file in the location", $SystemCSVPath)
+    $ServiceInformation |Export-Csv -Path $ServiceCSVPath -NoTypeInformation
 }
 Else {
-    Write-Error "The text file  was not found, check the path."
+    Write-Error "The text file was not found, please check the path."
 }
